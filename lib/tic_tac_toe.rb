6f3e3b3949ae06bc0b787f6683dc01543c756202
Lindsay Braviak <lbraviak@gmail.com>
2018-07-06 16:39:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,11 +54,12 @@ def turn_count(board)
     if item.downcase == "x" || item.downcase == "o"
       counter = counter + 1
     end
+    counter
   end
 end
 
 def current_player(board)
- turn_count(board) % 2 != 0 ? "X" : "O"
+ turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
 
